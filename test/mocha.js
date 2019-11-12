eb5c6a1dceb8e041fc37e0783a500e04d9a24bec
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2019-11-12 08:37:49 +0530

    updating

--- test/mocha.js
+++ test/mocha.js
@@ -1,7 +1,7 @@
 var nconf = require('nconf');
 
 nconf.overrides({
-    "SERVICE_CONFIGS": "master:accounts:/apis/v/configs",
+    "SERVICE_CONFIGS": "master:www:/apis/v/configs",
     "SERVICE_CLIENTS": "master:accounts:/apis/v/clients",
     "SERVICE_USERS": "master:accounts:/apis/v/users",
     "SERVICE_TOKENS": "master:accounts:/apis/v/tokens",
